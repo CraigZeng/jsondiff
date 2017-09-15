2d2808b5915c2a99974273c3d92749f7caca614c
zengcheng <zengchengcraig@didichuxing.com>
2017-09-15 19:09:41 +0800

    inited webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 
+class JSONDiff {
+    constructor() {
+        console.log('tesst');
+    }
+    renderDiff() {
+
+    }
+    renderToken() {
+
+    }
+    renderSame() {
+
+    }
+    render() {
+
+    }
+    renderJSON() {
+
+    }
+}
+
+module.exports = exports = JSONDiff;
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: ['webpack-dev-server/client?http://localhost:9000/', 'webpack/hot/dev-server', './src/index'],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'jsondiff.js',
+        publicPath: '/assets/',
+        library: 'JSONDiff',
+        libraryTarget: 'umd'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 9000
+    },
+    resolve: {
+        extensions: [".js", ".less"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'less-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: 'head',
+            filename: 'index.html',
+            template: 'index.html',
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin(),
+    ],
+    context: __dirname,
+    devtool: "source-map"
+};
