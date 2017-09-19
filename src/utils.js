30d54eff8cb2b75c14dac653d4156cda31355ccb
zengcheng <zengchengcraig@didichuxing.com>
2017-09-19 19:53:25 +0800

    add json format

---
+++ src/utils.js
@@ -0,0 +1,92 @@
+
+function typeIt(obj) {
+    var type = Object.prototype.toString.call(obj).slice(8, -1).toLowerCase();
+    return type;
+}
+
+function escapeHtml(str) {
+    return str;
+}
+
+function createJSONKey(key) {
+    return '<span class="json-key">' + escapeHtml(key) + '</span>';
+}
+
+function createJSONValue(type, value) {
+    var html = '';
+    value = escapeHtml(value);
+    switch (type) {
+        case 'number':
+            html = '<span class="json-value json-number">' + value + '</span>';
+            break;
+        case 'string':
+            html = '<span class="json-value json-string">' + value + '</span>';
+            break;
+        case 'null':
+            html = '<span class="json-value json-null">' + value + '</span>';
+            break;
+        default:
+            html = '<span class="json-value json-unknown">' + value + '</span>';
+            break;
+    }
+    return html;
+}
+
+function createJSONSplitToken(char) {
+    char = char || ':'
+    return '<span class="json-split-token">' + char + '</span>';
+}
+
+function createJSONBlockToken(position) {
+    position = position || 'start';
+    if (position === 'end') {
+        return '<span class="json-block-token json-block-token-end">}</span>';
+    } else {
+        return '<span class="json-block-token json-block-token-start">{</span>';
+    }
+}
+
+function createJSONArrayToken(position) {
+    position = position || 'start';
+    if (position === 'end') {
+        return '<span class="json-block-token json-array-token-end">]</span>';
+    } else {
+        return '<span class="json-block-token json-array-token-start">[</span>';
+    }
+}
+
+function formatJSON(jsonObj) {
+    var type = typeIt(jsonObj);
+    var html = '';
+    switch (type) {
+        case 'object':
+            var keys = Object.keys(json);
+            html = createJSONBlockToken('start') + keys.sort().map(function (key) {
+                return createJSONKey(key) + createJSONSplitToken() + formatJSON(jsonObj[key]);
+            }).join('') + createJSONBlockToken('end');
+            break;
+        case 'array':
+            html = createJSONArrayToken('start') + jsonObj.map(function (item) {
+                return formatJSON(item);
+            }).join('') + createJSONArrayToken('end');
+            break;
+        case 'number':
+            html = createJSONValue('number', json);
+            break;
+        case 'string':
+            html = createJSONValue('string', json);
+            break;
+        case 'null':
+            html = createJSONValue('null', json);
+            break;
+        default:
+            html = createJSONValue('', json);
+            break;
+    }
+}
+
+
+export default {
+    typeIt,
+    formatJSON
+}
